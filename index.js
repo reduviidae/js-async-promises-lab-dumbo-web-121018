bdaf1bbb3ef6d593ced957d116b32ec16fa9291d
reduviidae <wiljago@gmail.com>
2019-01-30 17:24:17 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,43 @@ const questions = [
 ]
 
 let question;
+const questionContainer = document.querySelector(".question-container");
+
+function appendQuestion(question) {
+  if (question) {
+    questionContainer.innerText = question.questionText;
+  }
+}
+
+function askQuestionThen(time) {
+  question = questions[0];
+  return new Promise((resolve, reject) => {
+    setTimeout(appendQuestion(question), time)
+  });
+}
+
+function removeQuestion() {
+  questionContainer.innerText = "";
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time)
+}
+
+function trueAndFalseButtons() {
+  return Array.from(document.querySelector(".true-false-list").getElementsByClassName("btn"))
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(button => {
+    if (button.classList.contains("hide")) {
+      button.classList.remove("hide")
+    } else {
+      button.classList.add("hide")
+    }
+  })
+}
+
+function displayQuestionOnClick(){
+
+}
